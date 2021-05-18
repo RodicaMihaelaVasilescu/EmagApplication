a7b82652298f8e4686517c8e133e1e7aabe7cb13
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2021-05-19 00:26:21 +0300

    add project

---
+++ EmagApplication/EmagApplication/Model/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace EmagApplication.Model
+{
+  class Item
+  {
+    public string Name { get; set; }
+    public BitmapImage Icon { get; set; }
+    public string Description { get; set; }
+    public string Category { get; set; }
+    public bool IsAvailable { get; set; } = true;
+    public double Price { get; set; }
+    public double Rating { get; set; }
+    public string Brand { get; set; }
+
+  }
+}
